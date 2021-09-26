89c41dcebfd94804be2cf60cf62a595567468af8
notmaurox <mauro.antoine.chavez@gmail.com>
2021-09-25 18:23:06 -0700

    model almost code complete, too add gaussian

--- src/learning_algorithms/k_nn_predictor.py
+++ src/learning_algorithms/k_nn_predictor.py
@@ -1,9 +1,17 @@
 import logging
 import sys
+import os, pathlib
 import pandas as pd
 import numpy as np
 
+from typing import List
 from statistics import mode
+
+import os, pathlib
+PATH_TO_SRC_DIR = os.path.join(pathlib.Path(__file__).parent.resolve(), "../", "src/")
+sys.path.insert(0, PATH_TO_SRC_DIR)
+
+from data_utils.metrics_evaluator import MetricsEvaluator
 
 # Logging stuff
 LOG = logging.getLogger(__name__)
@@ -14,11 +22,23 @@ LOG.setLevel(logging.DEBUG)
 # handler.setFormatter(formatter)
 # LOG.addHandler(handler)
 
+PRED_COL_NAME = "prediction"
+
+# TODO logging
 class KNearestNeighborPredictor(object):
 
-    @staticmethod
-    def k_nearest_neighbor(k: int, class_col: str, train_set: pd.DataFrame, test_set: pd.DataFrame) -> pd.DataFrame:
-        LOG.info(f"Running prediction by majority label on DataFrame column {class_col}")
+    # TODO comment
+    def __init__(self, k:int, do_regression:bool, allowed_error:float=None):
+        self.k = k
+        self.bandwidth_param = 0.00
+        self.allowed_error = allowed_error
+        self.training_set_sizes = []
+        self.classification_scores = []
+        self.do_regression = do_regression
+
+    # TODO comment
+    def k_nearest_neighbor(self, class_col: str, train_set: pd.DataFrame, test_set: pd.DataFrame) -> pd.DataFrame:
+        LOG.info(f"Running K nearest neighbor prediction by majority label on DataFrame column {class_col}")
         if class_col not in train_set.columns:
             raise ValueError(f'Column missing from dataframe: {class_col}')
         # Run prediction
@@ -26,12 +46,13 @@ class KNearestNeighborPredictor(object):
         for test_row_index in test_set.index:
             neighbors = []
             for train_row_index in train_set.index:
+                # Calculate Frobenius norm aka euclidean norm between two vectors
                 euclidean_dist = np.linalg.norm(
                     test_set.loc[test_row_index].drop([class_col])
                     - train_set.loc[train_row_index].drop([class_col])
                 )
                 # Lazy method for tracking distance of K NN and associated label. Could be improved with min heap
-                if len(neighbors) < k: 
+                if len(neighbors) < self.k: 
                     neighbors.append([euclidean_dist, train_set.loc[train_row_index][class_col]])
                 else:
                     for neighbor_index in range(len(neighbors)):
@@ -40,21 +61,128 @@ class KNearestNeighborPredictor(object):
                             neighbors[neighbor_index][1] = train_set.loc[train_row_index][class_col]
                             break
             predicted_classes.append(mode([neighbor[1] for neighbor in neighbors]))
-        test_set["predicted_class"] = pd.Series(predicted_classes)
+        # Need to use indicies of test set when adding prediction series to test set df
+        test_set[PRED_COL_NAME] = pd.Series(predicted_classes, index=test_set.index)
         return test_set
 
-    @staticmethod
-    def edited_k_nearest_neighbor(class_col: str, train_set: pd.DataFrame, test_set: pd.DataFrame) -> pd.DataFrame:
-        LOG.info(f"Running prediction by majority label on DataFrame column {class_col}")
+    # TODO comment
+    # Iterate through the training set and classify using the other points in the training set.
+    # If rmv_correct==False, then examples that are misclasified are removed from the set. 
+    # If rmv_correct==True, then examples that are correctly classified are removed from the set. 
+    def make_edited_k_nn_train_set(
+        self, class_col: str, train_set: pd.DataFrame, remove_correct: bool=False) -> List[int]:
+        LOG.info(f"Generating indicies for edited k nearest neighbot train set on DataFrame column {class_col}")
+        if class_col not in train_set.columns:
+            raise ValueError(f'Column missing from dataframe: {class_col}')
+        for train_set_index in train_set.index:
+            test_dp_df = train_set.loc[train_set_index].copy().to_frame().transpose()
+            train_set_wo_test = train_set[~train_set.index.isin([train_set_index])]
+            if len(train_set_wo_test.index) == 0:
+                return train_set_wo_test
+            single_train_set_pred = self.k_nearest_neighbor(
+                class_col, train_set_wo_test, test_dp_df
+            )
+            # Check if missclassified
+            was_missclassified = False
+            if self.allowed_error:
+                if (abs(single_train_set_pred.loc[train_set_index][PRED_COL_NAME]-single_train_set_pred.loc[train_set_index][class_col])
+                    > self.allowed_error):
+                    was_missclassified = True
+            else:
+                if single_train_set_pred.loc[train_set_index][PRED_COL_NAME] != single_train_set_pred.loc[train_set_index][class_col]:
+                    was_missclassified = True
+            # if removing miss classified and data point was misclassified
+            if not remove_correct and was_missclassified:
+                train_set = train_set.drop(train_set_index)
+            # if removing correctly classified and point was not misclassified
+            elif remove_correct and not was_missclassified:
+                train_set = train_set.drop(train_set_index)
+        # At the end of for loop, edited_train_set_indicies will contain list of incorrectly classified datapoints
+        return train_set
+        
+    #TODO comment
+    # Repeat untill performance on validation set no longer improves or no further points are removed.
+    def edited_k_nearest_neighbor(
+        self, class_col: str, train_set: pd.DataFrame, test_set: pd.DataFrame, remove_correct: bool=False) -> pd.DataFrame:
+        LOG.info(f"Running K nearest neighbor prediction by majority label on DataFrame column {class_col}")
         if class_col not in train_set.columns:
             raise ValueError(f'Column missing from dataframe: {class_col}')
         # Run prediction
-        return test_set
+        edited_training_set = train_set.copy()
+        self.training_set_sizes = [len(edited_training_set)]
+        self.classification_scores = [0.00]
+        best_classification = None
+        while True:
+            edited_training_set = self.make_edited_k_nn_train_set(
+                class_col, edited_training_set, remove_correct
+            )
+            print(len(edited_training_set))
+            predicted_test_set = self.k_nearest_neighbor(
+                class_col, edited_training_set, test_set
+            )
+            classification_score = MetricsEvaluator.calculate_classification_score(
+                predicted_test_set[class_col], predicted_test_set[PRED_COL_NAME]
+            )
+            print(classification_score)
+            # when an iteration fails to decrease training set size or improve classification score,
+            if (len(edited_training_set) >= self.training_set_sizes[-1]
+                or classification_score < self.classification_scores[-1]):
+                return best_classification
+            else:
+                self.training_set_sizes.append(len(edited_training_set))
+                self.classification_scores.append(classification_score)
+                best_classification = predicted_test_set.copy()
 
-    @staticmethod
-    def condensed_k_nearest_neighbor(class_col: str, train_set: pd.DataFrame, test_set: pd.DataFrame) -> pd.DataFrame:
-        LOG.info(f"Running prediction by majority label on DataFrame column {class_col}")
+    # TODO comment
+    # continue to iterate through training set in random order untill a full loop through the training set does not
+    # add any more points to the set of edited train set indicies
+    def make_condensed_k_nn_train_set(self, class_col: str, train_set: pd.DataFrame) -> List[int]:
+        LOG.info(f"Generating indicies for edited k nearest neighbot train set on DataFrame column {class_col}")
         if class_col not in train_set.columns:
             raise ValueError(f'Column missing from dataframe: {class_col}')
-        # Run prediction
-        return test_set
+        # Build train set...
+        edited_train_set_indicies = []
+        while True:
+            edited_train_set_size_pre_train_set_iteration = len(edited_train_set_indicies)
+            train_set = train_set.sample(frac=1) # Shuffle train set order
+            for train_set_index in train_set.index:
+                # Base case, set of edited train set indicies is empty...
+                if edited_train_set_indicies == []:
+                    edited_train_set_indicies.append(train_set_index)
+                    continue
+                closest_point_dist = float('inf')
+                closest_point_index = None
+                for edited_train_set_index in edited_train_set_indicies:
+                    euclidean_dist = np.linalg.norm(
+                        train_set.loc[train_set_index].drop([class_col])
+                        - train_set.loc[edited_train_set_index].drop([class_col])
+                    )
+                    if euclidean_dist < closest_point_dist:
+                        closest_point_dist = euclidean_dist
+                        closest_point_index = edited_train_set_index
+                print(train_set_index, closest_point_index)
+                if self.allowed_error:
+                    if (abs(train_set.loc[train_set_index][class_col]-train_set.loc[closest_point_index][class_col])
+                        > self.allowed_error):
+                        edited_train_set_indicies.append(train_set_index)
+                else:
+                    if train_set.loc[train_set_index][class_col] != train_set.loc[closest_point_index][class_col]:
+                        edited_train_set_indicies.append(train_set_index)
+            if len(edited_train_set_indicies) != edited_train_set_size_pre_train_set_iteration:
+                return edited_train_set_indicies
+
+    #TODO comment
+    def condensed_k_nearest_neighbor(self, class_col: str, train_set: pd.DataFrame, test_set: pd.DataFrame) -> pd.DataFrame:
+        LOG.info(f"Running K nearest neighbor prediction by majority label on DataFrame column {class_col}")
+        if class_col not in train_set.columns:
+            raise ValueError(f'Column missing from dataframe: {class_col}')
+        # Make condensed training set
+        edited_training_set_indicies = self.make_condensed_k_nn_train_set(
+            class_col, train_set
+        )
+        # Run using condensed training set
+        predicted_test_set = self.k_nearest_neighbor(
+            class_col, train_set.iloc[edited_training_set_indicies], test_set
+        )
+        return predicted_test_set
+
--- tests/test_learning_algorithms/test_k_nn_predictor.py
+++ tests/test_learning_algorithms/test_k_nn_predictor.py
@@ -12,7 +12,10 @@ from learning_algorithms.k_nn_predictor import KNearestNeighborPredictor
 
 class KNearestNeighborsPredictor(unittest.TestCase):
 
-    def test_majority_predictor_simple(self):
+    def test_k_nearest_neighbor(self):
+        knnp = KNearestNeighborPredictor(
+            4, False
+        )
         # train set
         cols = ["class", "int_field", "int_field"]
         data = [
@@ -33,11 +36,220 @@ class KNearestNeighborsPredictor(unittest.TestCase):
         ]
         test_df = pd.DataFrame(data, columns=cols)
         # run pred
-        pred_df = KNearestNeighborPredictor.k_nearest_neighbor(
-            4,
+        pred_df = knnp.k_nearest_neighbor("class", train_df, test_df)
+        print(pred_df)
+        self.assertTrue(pd.Series(["c1", "c2", "c3"]).equals(pred_df["prediction"]))
+
+    def test_make_edited_k_nn_train_set_rmv_misclassified(self):
+        knnp = KNearestNeighborPredictor(
+            1, False
+        )
+        # train set
+        cols = ["class", "int_field", "int_field"]
+        data = [
+            ["c1", 1, 1], #0
+            ["c2", 2, 2], #1
+            ["c3", 3, 3], #2
+        ]
+        train_df = pd.DataFrame(data, columns=cols)
+        df_indicies = knnp.make_edited_k_nn_train_set(
+            "class",
+            train_df,
+        )
+        # only one item from each class should end up in the train set...
+        print(df_indicies)
+        self.assertEqual(0, len(df_indicies))
+
+    def test_make_edited_k_nn_train_set_rmv_misclassified_empty(self):
+        knnp = KNearestNeighborPredictor(
+            1, False
+        )
+        # train set
+        cols = ["class", "int_field", "int_field"]
+        data = [
+            ["c1", 1, 1], #0
+            ["c2", 2, 2], #1
+            ["c3", 3, 3], #2
+        ]
+        train_df = pd.DataFrame(data, columns=cols)
+        df_indicies = knnp.make_edited_k_nn_train_set(
+            "class",
+            train_df,
+        )
+        # only one item from each class should end up in the train set...
+        print(df_indicies)
+        self.assertEqual(0, len(df_indicies))
+
+    def test_make_edited_k_nn_train_set_rmv_misclassified(self):
+        knnp = KNearestNeighborPredictor(
+            1, False
+        )
+        # train set
+        cols = ["class", "int_field", "int_field"]
+        data = [
+            ["c1", 1, 1], #0
+            ["c2", 2, 2], #1
+            ["c3", 3, 3], #2
+            ["c1", 1, 1], #0
+            ["c2", 2, 2], #1
+            ["c3", 3, 3], #2
+            ["c4", 1, 1], #0 - will be missclassified
+            ["c5", 2, 2], #1 - will be missclassified
+            ["c6", 3, 3], #2 - will be missclassified
+        ]
+        train_df = pd.DataFrame(data, columns=cols)
+        df_indicies = knnp.make_edited_k_nn_train_set(
+            "class",
+            train_df,
+        )
+        # only one item from each class should end up in the train set...
+        print(df_indicies)
+        self.assertEqual(6, len(df_indicies))
+
+    def test_make_edited_k_nn_train_set_rmv_correctly_classified(self):
+        knnp = KNearestNeighborPredictor(
+            1, False
+        )
+        # train set
+        cols = ["class", "int_field", "int_field"]
+        data = [
+            ["c1", 1, 1], #0
+            ["c2", 2, 2], #1
+            ["c3", 3, 3], #2
+            ["c1", 1, 1], #0
+            ["c2", 2, 2], #1
+            ["c3", 3, 3], #2
+        ]
+        train_df = pd.DataFrame(data, columns=cols)
+        print(train_df)
+        df_indicies = knnp.make_edited_k_nn_train_set(
+            "class",
+            train_df,
+            remove_correct=True
+        )
+        # only one item from each class should end up in the train set...
+        print(df_indicies)
+        self.assertEqual(3, len(df_indicies))
+
+    def test_edited_k_nearest_neighbor(self):
+        knnp = KNearestNeighborPredictor(
+            1, False
+        )
+        # train set
+        cols = ["class", "int_field", "int_field"]
+        data = [
+            ["c1", 1, 1], #0
+            ["c2", 2, 2], #1
+            ["c3", 3, 3], #2
+            ["c1", 1, 1], #0
+            ["c2", 2, 2], #1
+            ["c3", 3, 3], #2
+        ]
+        train_df = pd.DataFrame(data, columns=cols)
+        # test set
+        cols = ["class", "int_field", "int_field"]
+        data = [
+            ["c1", 1, 1], #0
+            ["c2", 2, 2], #1
+            ["c3", 3, 3], #2
+        ]
+        test_df = pd.DataFrame(data, columns=cols)
+        # run pred
+        pred_df = knnp.edited_k_nearest_neighbor(
+            "class",
+            train_df,
+            test_df,
+            remove_correct=True
+        )
+        print(pred_df)
+        print(knnp.training_set_sizes)
+        print(knnp.classification_scores)
+        self.assertTrue(pd.Series(["c1", "c2", "c3"]).equals(pred_df["prediction"]))
+
+    def test_make_condensed_k_nearest_neighbor_train_set(self):
+        knnp = KNearestNeighborPredictor(
+            None, False
+        )
+        # train set
+        cols = ["class", "int_field", "int_field"]
+        data = [
+            ["c1", 1, 1], #0
+            ["c2", 2, 2], #1
+            ["c3", 3, 3], #2
+            ["c1", 1, 1], #0
+            ["c2", 2, 2], #1
+            ["c3", 3, 3], #2
+        ]
+        train_df = pd.DataFrame(data, columns=cols)
+        df_indicies = knnp.make_condensed_k_nn_train_set(
+            "class",
+            train_df,
+        )
+        # only one item from each class should end up in the train set...
+        print(df_indicies)
+        self.assertEqual(3, len(df_indicies))
+
+    def test_make_condensed_k_nearest_neighbor_train_set_err(self):
+        knnp = KNearestNeighborPredictor(
+            None, False, 0.5
+        )
+        # train set
+        cols = ["class", "int_field", "int_field"]
+        data = [
+            [1, 1, 1], #0
+            [2, 2, 2], #1
+            [3, 3, 3], #2
+            [1, 1, 1], #0
+            [2, 2, 2], #1
+            [3, 3, 3], #2
+        ]
+        train_df = pd.DataFrame(data, columns=cols)
+        df_indicies = knnp.make_condensed_k_nn_train_set(
+            "class",
+            train_df
+        )
+        # only one item from each class should end up in the train set...
+        print(df_indicies)
+        self.assertEqual(3, len(df_indicies))
+        knnp.allowed_error = 3
+        df_indicies = knnp.make_condensed_k_nn_train_set(
+            "class",
+            train_df
+        )
+        # only one item from each class should end up in the train set...
+        print(df_indicies)
+        self.assertEqual(1, len(df_indicies))
+
+    def test_condensed_k_nearest_neighbor(self):
+        knnp = KNearestNeighborPredictor(
+            1, False
+        )
+        # train set
+        cols = ["class", "int_field", "int_field"]
+        data = [
+            ["c1", 1, 1], #0
+            ["c2", 2, 2], #1
+            ["c3", 3, 3], #2
+            ["c1", 1, 1], #0
+            ["c2", 2, 2], #1
+            ["c3", 3, 3], #2
+        ]
+        train_df = pd.DataFrame(data, columns=cols)
+        # test set
+        cols = ["class", "int_field", "int_field"]
+        data = [
+            ["c1", 1, 1], #0
+            ["c2", 2, 2], #1
+            ["c3", 3, 3], #2
+        ]
+        test_df = pd.DataFrame(data, columns=cols)
+        # run pred
+        pred_df = knnp.condensed_k_nearest_neighbor(
             "class",
             train_df,
             test_df
         )
         print(pred_df)
-        self.assertTrue(pd.Series(["c1", "c2", "c3"]).equals(pred_df["predicted_class"]))
+        print(knnp.training_set_sizes)
+        print(knnp.classification_scores)
+        self.assertTrue(pd.Series(["c1", "c2", "c3"]).equals(pred_df["prediction"]))
